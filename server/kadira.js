3785c629d9e2963ef9181a45ee46851e2dee9120
Dane Grant <danecando@gmail.com>
2014-12-09 21:08:47 -0500

    cleanup

--- server/kadira.js
+++ server/kadira.js
@@ -1,2 +1,6 @@
-//Kadira.connect('WW35hSKuzfXdByTxk', 'cdb1372a-c0c5-4e0b-a6c4-fdcd19103a4d')
+/**
+ * https://kadira.io/
+ * Performance Monitoring for Meteor
+ */
+
 Kadira.connect('tnP8Xo4gvsMBsjwrq', 'fbdf083e-d868-4550-a79e-935ec6227ebe')
--- server/publish.js
+++ server/publish.js
@@ -1,49 +1,27 @@
-Meteor.publish('test', function() {
-    return Test.find();
-});
-
-Test.allow({
-    insert: function() {
-        return true;
-    }
-});
-
 // Extend the user collection
 Accounts.onCreateUser(function(options, user) {
 
     // Keep default profile
-    user.profile = options.profile;
+    user.profile = options.profile
 
-    return user;
-});
+    return user
+})
 
 // Publish user data
 Meteor.publish('userData', function() {
-    return Meteor.users.find();
-});
+    return Meteor.users.find()
+})
 
 // User collection permissions
 Meteor.users.allow({
     insert: function(userId, doc) {
-        return doc._id === userId;
+        return doc._id === userId
     },
     update: function(userId, doc) {
-        return doc._id === userId;
-    }
-});
-
-
-// Following
-Meteor.publish('follow', function() {
-    return Follow.find();
-});
-
-Follow.allow({
-    insert: function(userId, doc) {
-        return true;
+        return doc._id === userId
     }
 })
 
 Meteor.publish('messages', function(){
-    return Message.find({}, {sort: {timestamp: -1}, limit: 20});
-});
+    return Message.find({}, {sort: {timestamp: -1}, limit: 20})
+})
