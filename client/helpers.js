9bcd60314fc6ebc0f741d00db29ba83235c203f2
olivierrr <oliviersinbox@gmail.com>
2014-12-11 11:24:26 -0800

    added css class to linkMetions helper

--- client/helpers.js
+++ client/helpers.js
@@ -1,12 +1,11 @@
 
 /**
  * matches @mentions in a chunk of text and replaces them with a link
- * @todo give it a css class
  */
 UI.registerHelper('linkMentions', function(context){
     if(context){
         var str = context.replace(/\B@[a-z0-9_-]+/gi, function(match){
-            return '<a href="/user/' + match.slice(1) + '">' + match + '</a>'
+            return '<a class="userLink" href="/user/' + match.slice(1) + '">' + match + '</a>'
         });
         return new Handlebars.SafeString(str);
     }
