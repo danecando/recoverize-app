cef9c0843a961305969dd33e210e8128414dcfc3
olivierrr <oliviersinbox@gmail.com>
2014-12-11 19:52:46 -0800

    new message count on /messages

--- client/templates/user/messages.js
+++ client/templates/user/messages.js
@@ -3,7 +3,12 @@ Template.messages.helpers({
     messagesList: function(){
         return Message.find().fetch().reduce(function(arr, message){
             var name = message.to === Meteor.user().username ? message.from : message.to
-            if(arr.indexOf(name) === -1 && name !== Meteor.user().username) arr.push(name)
+            if(!arr.some(function(obj){return obj.username===name}) && name !== Meteor.user().username){
+                arr.push({
+                    username: name, 
+                    newMessageCount: Notification.find({path:'/messages/'+name+'/', checked: false}).count()
+                })
+            }
             return arr
         }, [])
     }
