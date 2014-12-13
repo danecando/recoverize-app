bcbc5a434d24930e8e800085298300fd06345be1
olivierrr <oliviersinbox@gmail.com>
2014-12-12 16:07:13 -0800

    listOfUsers

--- client/templates/app/chat.js
+++ client/templates/app/chat.js
@@ -1,26 +1,29 @@
 
 Template.chat.events({
-    'keydown .input-area input': function (e) {
+    'keydown .input-area input': function(e){
         if(e.which === 13) {
             sendMessage();
         }
     },
-    'click .input-area button': function () {
+    'click .input-area button': function(){
         sendMessage();
     }
 });
 
 Template.chat.helpers({
-    messages: function() {
-        return Chat.find({}, {sort: {timestamp: +1}});
+    messages: function(){
+        return Chat.find({}, {sort: {timestamp: +1}})
+    },
+    listOfUsers: function(){
+        return Presences.find()
     }
 });
 
-function sendMessage() {
+function sendMessage(){
     var input = $('.input-area input');
 
     if(input.val().trim() !== '') {
-        Meteor.call('addChat', input.val());
+        Meteor.call('addChat', input.val())
         input.val('')
     }
 }
