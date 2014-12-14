fb0dc2b943e0e01fcb4ea8c7090377f2bdb8b47c
olivierrr <oliviersinbox@gmail.com>
2014-12-13 22:04:56 -0800

    refactoring and commenting message

--- client/templates/user/message.js
+++ client/templates/user/message.js
@@ -1,4 +1,9 @@
 
+/**
+ * this template expects data.username
+ * data.username is the username of the user currentUser is chatting with
+ */
+
 Template.message.events({
     'keydown .input-area input': function(e){
         if(e.which === 13) {
@@ -8,54 +13,76 @@ Template.message.events({
     'click .input-area button': function(){
         sendMessage(this.username)
     },
-    'click .loadOlderMessages': function(){
-        var oldestBucket = MessageBuckets.findOne(
-            {$and: [{members: Meteor.user().username}, {members: this.username}]},  
-            {sort: {num: +1}}
-        )
-        if(oldestBucket){
-            oldestBucket = oldestBucket.num
-        }
-        Meteor.subscribe('message', this.username, oldestBucket-1)
+    'click .loadOlderPage': function(){
+        Meteor.subscribe('message', this.username, getOldestPage(this.username) -1)
     }
 })
 
 Template.message.rendered = function(){
     // make sure an user doesn't chat with himself
-    // if(Meteor.user() && this.data.username === Meteor.user().username){
-    //     Router.go('/messages')
-    // }
+    // @todo handle this before this template is even rendered
+    if(Meteor.user() && this.data.username === Meteor.user().username){
+        Router.go('/messages')
+    }
 }
 
 Template.message.helpers({
+
+    /**
+     * combines message arrays from various pages into a single message array
+     * @return {Array}
+     */
     messages: function() {
         return MessageBuckets.find({$and: [{members: Meteor.user().username}, {members: this.username}]}, {sort: {num: +1}})
             .fetch()
-            .reduce(function(arr, bucket){
-                bucket.messages.forEach(function(message){
+            .reduce(function(arr, page){
+                page.messages.forEach(function(message){
                     arr.push(message)
                 })
                 return arr
             }, [])
     },
-    moreResults: function(){
-        var oldestBucket = MessageBuckets.findOne(
-            {$and: [{members: Meteor.user().username}, {members: this.username}]},  
-            {sort: {num: +1}}
-        )
-        if(oldestBucket){
-            return oldestBucket.num > 0
-        }else{
-            return false
-        }
+
+    /**
+     * is there another page of messages we can load?
+     * @return {Boolean}
+     */
+    olderPages: function(){
+        return (getOldestPage(this.username) > 0)
     }
+
 })
 
+/**
+ * @param {String} username of message recipient
+ */
 function sendMessage(to) {
-    var input = $('.input-area input');
+    if(!to) return
 
+    var input = $('.input-area input')
     if(input.val().trim() !== '') {
         Meteor.call('addMessage', input.val(), to)
         input.val('')
+    }
+}
+
+/**
+ * returns the oldest messages page between username and currentUser loaded by client
+ *
+ * @param {String}
+ * @return {Number}
+ */
+function getOldestPage(username){
+    if(!Meteor.user()) return 0
+
+    var oldestBucket = MessageBuckets.findOne(
+        {$and: [{members: Meteor.user().username}, {members: username}]},  
+        {sort: {num: +1}}
+    )
+
+    if(oldestBucket){
+        return oldestBucket.num
+    }else{
+        return 0
     }
 }
