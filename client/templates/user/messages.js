dfe7a11ad08a67ddf3f9218876f976eca20fa4c2
olivierrr <oliviersinbox@gmail.com>
2014-12-14 14:54:05 -0800

    lastMessage stuff

--- client/templates/user/messages.js
+++ client/templates/user/messages.js
@@ -2,11 +2,28 @@
 Template.messages.helpers({
     messagesList: function(){
         return MessageSessions.find().map(function(session){
-            var otherMember = session.members.indexOf(Meteor.user().username) === 0 ? session.members[1] : session.members[0]
+            var otherMember = getOtherMember(session.members[0], session.members[1])
+            Meteor.subscribe('message', otherMember)
             return {
                 username: otherMember,
-                unreadMessageCount: Notification.find({path:'/messages/'+otherMember+'/', checked: false}).count()
+                unreadMessageCount: Notification.find({path:'/messages/'+otherMember+'/', checked: false}).count(),
+                totalMessageCount: session.messageCount,
+                lastMessage: getLastMessage(otherMember)
             }
         })
     }
 })
+
+function getOtherMember(member1, member2){
+    return member1 === Meteor.user().username ? member2 : member1
+}
+
+function getLastMessage(otherMember){
+    var lastMsg = MessageBuckets.findOne({$and: [{members: Meteor.user().username}, {members: otherMember}]})
+    if(lastMsg){
+        return lastMsg.messages[lastMsg.messages.length-1]
+    }
+    else{
+        return null
+    }
+}
