deaed74d54869f3372032c61a37de89b473bde7c
Dane Grant <danecando@gmail.com>
2014-12-19 00:37:06 -0500

    some chat styling, work in progress

--- client/templates/app/chat.js
+++ client/templates/app/chat.js
@@ -1,18 +1,36 @@
 
+Template.chat.rendered = function(template) {
+    $('#page').addClass('chat')
+    var $chatWindow = document.querySelector('.chat-area')
+    $chatWindow.scrollTop = $chatWindow.scrollHeight
+};
+
+Template.chat.destroyed = function() {
+    $('#page').removeclass('chat')
+}
+
 Template.chat.events({
-    'keydown .input-area input': function(e){
+    'keydown .chat-input input': function(e){
         if(e.which === 13) {
             sendMessage();
         }
     },
-    'click .input-area button': function(){
+    'click .chat-input button': function(){
         sendMessage();
+    },
+    'click .ul-btn': function(event, template) {
+        template.$('.chat-container').toggleClass('ul-open');
     }
 });
 
 Template.chat.helpers({
-    messages: function(){
-        return Chat.find({}, {sort: {timestamp: +1}})
+    messages: function(event, template) {
+        var messages = Chat.find({}, {sort: {timestamp: +1}})
+        messages.observeChanges({
+            added: function () {
+            }
+        })
+        return messages
     },
     listOfUsers: function(){
         return Presences.find()
@@ -20,7 +38,7 @@ Template.chat.helpers({
 });
 
 function sendMessage(){
-    var input = $('.input-area input');
+    var input = $('.chat-input input');
 
     if(input.val().trim() !== '') {
         Meteor.call('addChat', input.val())
