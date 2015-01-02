32f85724a03d6c2afc6518fa9ed7c39ad3358868
olivierrr <oliviersinbox@gmail.com>
2015-01-01 16:37:28 -0800

    basic filtering

--- client/templates/user/user-list.js
+++ client/templates/user/user-list.js
@@ -1,6 +1,20 @@
 
+Session.set('userList-filter', {})
+
 Template.userlist.helpers({
     listOfUsers: function(){
-        return Meteor.users.find()
+        return Meteor.users.find(Session.get('userList-filter'))
     }
+})
+
+Template.userlist.events({
+    'keyup .userList-filter': function(e){
+        var value = $(e.target).val().trim().toLowerCase()
+
+        if(value) {
+            Session.set('userList-filter', {username: value})
+        } else {
+            Session.set('userList-filter', {})
+        }
+    },
 })
