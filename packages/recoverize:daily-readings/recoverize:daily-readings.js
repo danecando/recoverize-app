20d99d0de17789ab5c59a1b6a30a2a1b359a910a
Dane Grant <danecando@gmail.com>
2015-01-15 22:27:21 -0500

    idnowutimdoing

--- packages/recoverize:daily-readings/recoverize:daily-readings.js
+++ packages/recoverize:daily-readings/recoverize:daily-readings.js
@@ -1,1 +1,18 @@
-// Write your package code here!
+DailyReadings = {}
+
+/**
+ * Server code
+ */
+if (Meteor.isServer) {
+    DailyReadings.loadReading = function(book, date) {
+
+        var fs = Npm.require('fs')
+        var bookPath = process.env.PWD + '/server/books/'
+
+        var dateString = ((date.getMonth()+1) + "-" + date.getDay())
+        console.log(dateString)
+
+
+        return fs.readFileSync(bookPath + book)
+    }
+}
