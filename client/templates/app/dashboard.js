96cf8f5f163c035174a4989c331d5c889fcbc93c
Dane Grant <danecando@gmail.com>
2015-02-08 15:36:42 -0500

    basic dashboard

--- client/templates/app/dashboard.js
+++ client/templates/app/dashboard.js
@@ -1,13 +1,27 @@
 
 Template.dashboard.helpers({
-
+    todaysDate: function() {
+        return moment().format('dddd, MMMM Do YYYY')
+    },
+    user: function() {
+        return Meteor.user()
+    },
+    notifications: function(){
+        var notify = Notification.find({type: {$not: 'message'}})
+        if (notify.count()) return notify
+        else return false
+    },
+    tasks: function() {
+        return Tasks.find({}, { sort: { position: 1 }}).map(function(task, index) {
+            task.index = index
+            return task
+        })
+    },
+    completed: function() {
+        return Tasks.find({ checked: true })
+    }
 })
 
 Template.dashboard.events({
 
 })
-
-
-//if (Meteor.user()) {
-//    console.log(Meteor.user());
-//}
