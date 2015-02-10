ba58ab3e320b78e72b6bd8ca4aa797c84f679c10
Dane Grant <danecando@gmail.com>
2015-02-09 22:01:12 -0500

    help page

--- client/templates/app/help.js
+++ client/templates/app/help.js
@@ -7,8 +7,16 @@ Template.help.events({
     'click .get-help': function(e, template) {
         e.preventDefault()
 
+        var fromTop = ($(window).width() > 768) ? 0 : 43
+
         $('#page').animate({
-            scrollTop: $('#get-help').offset().top
+            scrollTop: $('#get-help').offset().top - fromTop
         }, 500)
+    },
+    'click .submit-number button': function(e, template) {
+        e.preventDefault()
+
+        var number = $('#userPhone').val()
+        Meteor.call('sendText', number)
     }
 })
---
+++ server/methods.js
@@ -0,0 +1,13 @@
+Meteor.methods({
+    'sendText': function(number) {
+        twilio = Twilio('AC77278612a7d03f1e94d65af71206ccea', '464fa853fd981daad24dc68b29d999c2')
+        twilio.sendSms({
+            to: '+19548260058',
+            from: '+14697131773',
+            body: 'Call ' + number + '. They need help - recoverize app'
+        }, function (err, responseData) {
+            console.log(err)
+            console.log(responseData)
+        })
+    }
+})
