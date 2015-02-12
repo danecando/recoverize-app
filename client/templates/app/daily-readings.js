934bc4e792e14fd9913e17c3342213aaa118c81d
Dane Grant <danecando@gmail.com>
2015-02-11 22:20:13 -0500

    updated reading schema

--- client/templates/app/daily-readings.js
+++ client/templates/app/daily-readings.js
@@ -1,7 +1,3 @@
 Template.dailyReadings.helpers({
-    justForToday: function() {
-        var date = new Date()
-        var dateString = (date.getMonth()+1) + '-' + date.getDay()
-        return JustForToday.find({ date: dateString })
-    }
+
 })
