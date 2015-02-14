b317a7bca42b7d962a777eace3c539c8fbce8c65
Dane Grant <danecando@gmail.com>
2015-02-13 23:23:50 -0500

    daily reading page style / function

--- client/templates/app/daily-readings.js
+++ client/templates/app/daily-readings.js
@@ -1,5 +1,20 @@
+Template.dailyReadings.created = function() {
+
+    this.currentReading = new ReactiveVar('justForToday');
+}
+
 Template.dailyReadings.helpers({
-    todaysReadings: function() {
-        return Readings.findOne();
+    currentReading: function() {
+        var readings = Readings.findOne();
+        return readings[Template.instance().currentReading.get()];
+    },
+    dateString: function() {
+      return moment().format('MMMM Do');
     }
 });
+
+Template.dailyReadings.events({
+    'change #readingSelect': function(e, template) {
+        template.currentReading.set(e.target.value);
+    }
+})
