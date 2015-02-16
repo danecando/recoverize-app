9e295e225c25bf7725e876037e236a26b1687f1f
Dane Grant <danecando@gmail.com>
2015-02-16 11:51:08 -0500

    fixed status not clearing

--- client/templates/social/new-status.js
+++ client/templates/social/new-status.js
@@ -1,76 +1,79 @@
 Template.newStatus.created = function() {
     this.charsLeft = new ReactiveVar(255);
-}
+};
+
 Template.newStatus.rendered = function() {
     var chars = $('#newStatus').val().length;
     $('#charsLeft').css({ color: 'rgb(' + chars + ',20,20)' });
     this.charsLeft.set(255 - chars);
-}
+};
 
 Template.newStatus.destroyed = function() {
     Session.set('statusReply', null);
-}
+};
 
 Template.newStatus.helpers({
     replyData: function() {
         var reply = Session.get('statusReply');
-        if (reply) return reply;
-        else return false;
+
+        if (reply) {
+            return reply;
+        } else {
+            return false;
+        }
     },
     charsLeft: function() {
-        return Template.instance().charsLeft.get()
+        return Template.instance().charsLeft.get();
     },
     status: function() {
-        return Status.find({username: Meteor.user().username}, {sort: {timestamp: -1}, limit: 1})
+        return Status.find({username: Meteor.user().username}, {sort: {timestamp: -1}, limit: 1});
     }
-})
+});
 
 Template.newStatus.events({
     'keypress #newStatus': function(e, template) {
-        var status = e.target.value
+        var status = e.target.value;
         if(e.which == 13 && isValidStatus(status)){
-            e.preventDefault()
-            submitStatus(e, template)
+            e.preventDefault();
+            submitStatus(e, template);
         }
     },
     'submit .share-form form': function(e, template) {
-        e.preventDefault()
-        submitStatus(e, template)
-
+        e.preventDefault();
+        submitStatus(e, template);
     },
     'change #newStatus, keypress #newStatus, keyup #newStatus, keydown #newStatus': function(e, template) {
-        var chars = $(e.target).val().length
-        $('#charsLeft').css({ color: 'rgb(' + chars + ',20,20)' })
-        template.charsLeft.set(255 - chars)
+        var chars = $(e.target).val().length;
+        $('#charsLeft').css({ color: 'rgb(' + chars + ',20,20)' });
+        template.charsLeft.set(255 - chars);
     },
     'click #getImage': function(e, template) {
-        e.preventDefault()
-        $('#statusImage').focus().trigger('click')
+        e.preventDefault();
+        $('#statusImage').focus().trigger('click');
     },
     'change #statusImage': function(e, template) {
         $('.add-image').fadeOut(100, function() {
-            $('.remove-image').fadeIn(100)
-            $('.file-name').text(e.target.value.replace(/^.*[\\\/]/, ''))
-        })
+            $('.remove-image').fadeIn(100);
+            $('.file-name').text(e.target.value.replace(/^.*[\\\/]/, ''));
+        });
     },
     'click #removeImage': function(e, template) {
-        e.preventDefault()
-        $('#statusImage').val('')
+        e.preventDefault();
+        $('#statusImage').val('');
         $('.remove-image').fadeOut(100, function() {
-            $('.add-image').fadeIn(100)
-        })
+            $('.add-image').fadeIn(100);
+        });
     }
-})
+});
 
-function isValidStatus(str){
-    return str.trim() !== ''
-        && str.length < 255
+function isValidStatus(str) {
+    return str.trim() !== '' && str.length < 255;
 }
 
 function submitStatus(e, template) {
-    var share = Object.create(null)
-    share.status = $('#newStatus').val()
-    share.image = template.$('#statusImage')[0].files[0] || null
+    var share = Object.create(null);
+    share.status = $('#newStatus').val();
+    share.image = template.$('#statusImage')[0].files[0] || null;
 
     if(isValidStatus(share.status) || share.image) {
         if (!Meteor.isCordova && share.image) {
@@ -82,23 +85,25 @@ function submitStatus(e, template) {
                     return;
                 }
 
-                share.image = Meteor.user().username + '/' + share.image.name
+                share.image = Meteor.user().username + '/' + share.image.name;
+
                 Meteor.call('createStatus', share, function(error, result) {
-                    $('#newStatus, #statusImage').val('')
-                    $("#page").animate({ scrollTop: $(document).height() }, 200)
+                    $('#newStatus, #statusImage').val('');
+                    $("#page").animate({ scrollTop: $(document).height() }, 200);
                     $('.remove-image').fadeOut(100, function() {
-                        $('.add-image').fadeIn(100)
-                    })
-                })
-            })
+                        $('.add-image').fadeIn(100);
+                    });
+                });
+            });
+
         } else {
             Meteor.call('createStatus', share, function(error, result) {
-                $('#newStatus', '#statusImage').val('')
-                $("#page").animate({ scrollTop: $(document).height() }, 200)
+                $('#newStatus, #statusImage').val('');
+                $("#page").animate({ scrollTop: $(document).height() }, 200);
                 $('.remove-image').fadeOut(100, function() {
-                    $('.add-image').fadeIn(100)
-                })
-            })
+                    $('.add-image').fadeIn(100);
+                });
+            });
         }
     }
 }
