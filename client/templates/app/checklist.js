dc12d976268bda95ff6e9e91430987335921c735
Dane Grant <danecando@gmail.com>
2015-02-16 00:20:56 -0500

    fixed task submit bug #58

--- client/templates/app/checklist.js
+++ client/templates/app/checklist.js
@@ -1,41 +1,53 @@
 Template.checklist.helpers({
     tasks: function() {
         return Tasks.find({}, { sort: { position: 1 }}).map(function(task, index) {
-            task.index = index
-            return task
-        })
+            task.index = index;
+            return task;
+        });
     },
     completed: function() {
-        return Tasks.find({ checked: true })
+        return Tasks.find({ checked: true });
     }
-})
+});
 
 Template.checklist.events({
     'change [type=checkbox]': function(e) {
-        var id = $(e.target).closest('.task').attr('data-taskId')
+
+        var id = $(e.target).closest('.task').attr('data-taskId');
+
         if ($(e.target).prop('checked')) {
-            Meteor.call('checkTask', id)
+            Meteor.call('checkTask', id);
         } else {
-            Meteor.call('uncheckTask', id)
+            Meteor.call('uncheckTask', id);
         }
     },
     'submit #task-form form': function(e) {
-        e.preventDefault()
-        var task = $('[name=task]').val()
-        var position = $('.task-list li').length+1
-        Meteor.call('addTask', task, position)
-        $('[name=task]').val('')
+        e.preventDefault();
+
+        var task = $('[name=task]').val();
+        if (!task) {
+            return;
+        }
+
+        var position = $('.task-list li').length+1;
+
+        Meteor.call('addTask', task, position);
+
+        $('[name=task]').val('');
     },
     'click #add-new': function(e) {
-        $('#task-form').slideToggle(250)
-        $('#newTask').focus()
+
+        $('#task-form').slideToggle(250);
+        $('#newTask').focus();
     },
     'click .delete-task': function(e) {
+
         if (confirm('Are you sure you want to delete this item?')) {
-            Meteor.call('deleteTask', $(e.target).closest('.task').attr('data-taskId'))
+            Meteor.call('deleteTask', $(e.target).closest('.task').attr('data-taskId'));
         }
     },
     'click #reset-all': function(e) {
-        Meteor.call('resetTasks')
+
+        Meteor.call('resetTasks');
     }
-})
+});
