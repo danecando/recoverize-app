bc5b5c15dc845b754e4d8f1481b47931f3bcf96c
Dane Grant <danecando@gmail.com>
2015-02-25 19:29:53 -0500

    attempted sign in page bug fix

--- client/templates/auth/signin.js
+++ client/templates/auth/signin.js
@@ -1,76 +1,73 @@
 
-Template.signin.created = function() {
-}
-
-Template.signin.destroyed = function() {
-}
-
 Template.signin.helpers({
-    userCount: function(){
-        return Meteor.users.find().count()
+    userCount: function() {
+        return Meteor.users.find().count();
     }
-})
+});
 
 Template.signin.events({
-    'click #facebook-login': function(event, template) {
+    'click #facebook-login': function(e, template) {
         Meteor.loginWithFacebook({ requestPermissions: ['email']},
         function(error) {
             if (error) {
-                template.$('.response').addClass('error').text("Couldn't log you in with Facebook")
-                return
+                $('.response').addClass('error').text("Couldn't log you in with Facebook");
+                return;
             }
 
             if (!Meteor.user().profileCreated && !Roles.userIsInRole(Meteor.user(), ['pending'])) {
-                Meteor.call('setUserRole', Meteor.user()._id, ['pending'])
-                Router.go('/create-profile')
+                Meteor.call('setUserRole', Meteor.user()._id, ['pending']);
+                Router.go('/create-profile');
             } else {
-                Router.go('/')
+                Router.go('/');
             }
-        })
+        });
     },
-    'click #twitter-login': function(event, template) {
+    'click #twitter-login': function(e, template) {
         Meteor.loginWithTwitter(function(error) {
             if (error) {
-                template.$('.response').addClass('error').text("Couldn't log you in with Twitter")
-                return
+                $('.response').addClass('error').text("Couldn't log you in with Twitter");
+                return;
             }
 
             if (!Meteor.user().profileCreated && !Roles.userIsInRole(Meteor.user(), ['pending'])) {
-                Meteor.call('setUserRole', Meteor.user()._id, ['pending'])
-                Router.go('/create-profile')
+                Meteor.call('setUserRole', Meteor.user()._id, ['pending']);
+                Router.go('/create-profile');
             } else {
-                Router.go('/')
+                Router.go('/');
             }
-        })
+        });
     },
-    'submit': function(event, template) {
-        event.preventDefault()
+    'submit form': function(e, template) {
+        e.preventDefault();
 
-        var email = template.$('[name=email]').removeClass('input-error').val()
-        var password = template.$('[name=password]').val()
+        var email = $('[name=email]').removeClass('input-error').val();
+        var password = $('[name=password]').val();
 
-        var errors = {}
+        var errors = {};
 
         if (!email) {
-            errors.email = 'Email required'
-            template.$('[name=email]').addClass('input-error')
-            template.$('.response').addClass('error').text(errors.email)
+            errors.email = 'Email required';
+            $('[name=email]').addClass('input-error');
+            $('.response').addClass('error').text(errors.email);
         }
 
         if (!password && email) {
-            errors.password = 'Password required'
-            template.$('[name=password]').addClass('input-error')
-            template.$('.response').addClass('error').text(errors.password)
+            errors.password = 'Password required';
+            $('[name=password]').addClass('input-error');
+            $('.response').addClass('error').text(errors.password);
         }
 
         if (_.keys(errors).length) {
-            return
+            return;
         }
 
-        Meteor.loginWithPassword(email, password, function(error) {
-            if (error) template.$('.response').addClass('error').text('incorrect username/password')
+        Meteor.loginWithPassword({ email: email }, password, function(error) {
+            if (error) {
+                $('.response').addClass('error').text('incorrect username/password');
+                return;
+            }
 
-            Router.go('/')
-        })
+            Router.go('/');
+        });
     }
-})
+});
