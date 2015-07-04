443ae8338d31c83aef572f3133d370c8b011f3a5
Dane Grant <danecando@gmail.com>
2015-07-04 19:06:10 -0400

    scroll to input on focus

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,4 +1,3 @@
-
 var MENU_KEY = 'menuOpen';
 Session.setDefault(MENU_KEY, false);
 
@@ -9,68 +8,76 @@ var CONNECTION_ISSUE_TIMEOUT = 5000;
 
 Meteor.startup(function() {
 
-    // set up a swipe left / right handler
-    $(document.body).touchwipe({
-        wipeLeft: function () {
-            Session.set(MENU_KEY, false);
-        },
-        wipeRight: function () {
-            Session.set(MENU_KEY, true);
-        },
-        preventDefaultEvents: false
-    });
+  // set up a swipe left / right handler
+  $(document.body).touchwipe({
+    wipeLeft: function() {
+      Session.set(MENU_KEY, false);
+    },
+    wipeRight: function() {
+      Session.set(MENU_KEY, true);
+    },
+    preventDefaultEvents: false
+  });
 
-    setTimeout(function () {
-        // Launch screen handle created in lib/router.js
-        dataReadyHold.release();
+  setTimeout(function() {
+    // Launch screen handle created in lib/router.js
+    dataReadyHold.release();
 
-        // Show the connection error box
-        Session.set(SHOW_CONNECTION_ISSUE_KEY, true);
-    }, CONNECTION_ISSUE_TIMEOUT);
+    // Show the connection error box
+    Session.set(SHOW_CONNECTION_ISSUE_KEY, true);
+  }, CONNECTION_ISSUE_TIMEOUT);
 
 });
 
 Template.layout.rendered = function() {
-    $('.user-scroll').on('touchstart', function(event){});
-    var $viewportMeta = $('meta[name="viewport"]');
-    $('input, textarea').bind('focus blur', function(event) {
-        $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' +        (event.type == 'blur' ? 10 : 1));
-    });
+  $('.user-scroll').on('touchstart', function(event) {
+  });
+  var $viewportMeta = $('meta[name="viewport"]');
+  $('input, textarea').bind('focus blur', function(event) {
+    $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+  });
 };
 
 /**
  * Helpers
  */
 Template.layout.helpers({
-    menuOpen: function() {
-        return Session.get(MENU_KEY) && 'menu-open';
-    },
-    cordova: function() {
-        return Meteor.isCordova && 'cordova';
-    },
-    connected: function() {
-        if (Session.get(SHOW_CONNECTION_ISSUE_KEY)) {
-            return Meteor.status().connected;
-        } else {
-            return true;
-        }
+  menuOpen: function() {
+    return Session.get(MENU_KEY) && 'menu-open';
+  },
+  cordova: function() {
+    return Meteor.isCordova && 'cordova';
+  },
+  connected: function() {
+    if (Session.get(SHOW_CONNECTION_ISSUE_KEY)) {
+      return Meteor.status().connected;
+    } else {
+      return true;
     }
+  }
 });
 
 /**
  * Events
  */
 Template.layout.events({
-    'click .menu-toggle': function() {
-        Session.set(MENU_KEY, !Session.get(MENU_KEY));
-    },
-    'click #page-overlay': function(event) {
-        Session.set(MENU_KEY, false);
-        event.preventDefault();
-    },
-    'click #menu nav a, click .notification-nav a': function() {
-        Session.set(MENU_KEY, false);
-    }
+  'click .menu-toggle': function() {
+    Session.set(MENU_KEY, !Session.get(MENU_KEY));
+  },
+  'click #page-overlay': function(event) {
+    Session.set(MENU_KEY, false);
+    event.preventDefault();
+  },
+  'click #menu nav a, click .notification-nav a': function() {
+    Session.set(MENU_KEY, false);
+  },
+  'focus .message-input textarea, focus .chat-input textarea': function() {
+    setTimeout(function() {
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 300);
+    }, 100);
+  }
 });
 
 //Transitioner.default({
