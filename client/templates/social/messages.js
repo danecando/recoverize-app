2889b1322ae7e31199e42fe1cc5db4bca91b5d7c
Dane Grant <danecando@gmail.com>
2015-07-05 19:12:59 -0400

    temporary fix for messages bug

--- client/templates/social/messages.js
+++ client/templates/social/messages.js
@@ -1,31 +1,31 @@
 
 Template.messages.helpers({
-    messagesList: function(){
-        return MessageSessions.find().map(function(session){
-            var otherMember = getOtherMember(session.members[0], session.members[1])
-            return {
-                username: otherMember,
-                unreadMessageCount: Notification.find({path:'/messages/'+otherMember+'/', checked: false}).count(),
-                totalMessageCount: session.messageCount,
-                lastMessage: getLastMessage(otherMember)
-            }
-        }).sort(function(b, a){
-            return a.lastMessage.timestamp - b.lastMessage.timestamp
-        })
-    }
+  messagesList: function(){
+    return MessageSessions.find().map(function(session){
+      var otherMember = getOtherMember(session.members[0], session.members[1])
+      return {
+        username: otherMember,
+        unreadMessageCount: Notification.find({path:'/messages/'+otherMember+'/', checked: false}).count(),
+        totalMessageCount: session.messageCount,
+        lastMessage: getLastMessage(otherMember)
+      }
+    }).sort(function(b, a){
+      return a.lastMessage.timestamp - b.lastMessage.timestamp
+    })
+  }
 })
 
 function getOtherMember(member1, member2){
-    return member1 === Meteor.user().username ? member2 : member1
+  return member1 === Meteor.user().username ? member2 : member1
 }
 
 function getLastMessage(otherMember){
-    var lastMsg = MessageBuckets.findOne({$and: [{members: Meteor.user().username}, {members: otherMember}]})
-    if(lastMsg){
-        return lastMsg.messages[lastMsg.messages.length-1]
-    }
-    else{
-        // messageList helper requires a timestamp!
-        return {timestamp:0}
-    }
+  var lastMsg = MessageBuckets.findOne({$and: [{members: Meteor.user().username}, {members: otherMember}]})
+  if(lastMsg){
+    return lastMsg.messages[lastMsg.messages.length-1]
+  }
+  else{
+    // messageList helper requires a timestamp!
+    return {timestamp:0}
+  }
 }
